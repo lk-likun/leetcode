e9958f7eded61879df53b635cccc54b6d1bbff0c
lik <lk_likun@126.com>
2022-04-08 16:55:22 +0800

    移除元素

--- editor/cn/[27]移除元素.py
+++ editor/cn/[27]移除元素.py
@@ -3,20 +3,16 @@ from typing import List
 
 
 class Solution:
-    @staticmethod
-    def removeElement(nums: List[int], val: int) -> int:
-        pass
+    def removeElement(self, nums: List[int], val: int) -> int:
+        a = 0
+        b = 0
 
+        while a < len(nums):
+            if nums[a] != val:
+                nums[b] = nums[a]
+                b += 1
+            a += 1
 
-nums1 = [0, 1, 2, 2, 3, 0, 4, 2]
-val1 = 2
-x = Solution.removeElement(nums1, val1)
-print(x)
-# if __name__ == '__main__':
-#     nums1 = ['1', '2', '3']
-#     val1 = 2
-#     x = nums1.remove('2')
-#     print(nums1)
-#     print(len(nums1))
-#     print(x)
+        return b
+
 # leetcode submit region end(Prohibit modification and deletion)
