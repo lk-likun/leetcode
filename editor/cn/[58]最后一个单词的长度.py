84f9c267c79f8b0197cf3eba6244640870f2fee9
lik <lk_likun@126.com>
2022-04-12 21:00:22 +0800

    最后一个单词的长度

--- editor/cn/[58]最后一个单词的长度.py
+++ editor/cn/[58]最后一个单词的长度.py
@@ -1,6 +1,15 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
+        if len(s) == 1 or (s[-1].isalpha() and s[-2] == ' '):
+            return 1
+        if s[-1].isalpha():
+            return 1 + self.lengthOfLastWord(s[:-1])
+        return self.lengthOfLastWord(s[:-1])
+        # return len(s.split()[-1])
 # leetcode submit region end(Prohibit modification and deletion)
 
+
+# x = " fly me to the moon "
+# y = Solution().lengthOfLastWord(x)
+# print(y)
