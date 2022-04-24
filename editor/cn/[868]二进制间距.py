ba7329b049b0775c4edf19010356a8dae6789fe4
unknown <lk_likun@126.com>
2022-04-24 23:32:27 +0800

    383

---
+++ editor/cn/[868]二进制间距.py
@@ -0,0 +1,13 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        tmp = bin(n)[2:]
+        left = 0
+        res = 0
+        for i in range(len(tmp)):
+            if tmp[i] == '1':
+                res = max(res, i - left)
+                left = i
+        return res
+
+# leetcode submit region end(Prohibit modification and deletion)
