bd49deb2bf4a77f19a09def8a8194773f21b2da0
lik <lk_likun@126.com>
2022-04-26 20:23:46 +0800

    [844]比较含退格的字符串

---
+++ editor/cn/[844]比较含退格的字符串.py
@@ -0,0 +1,24 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def backspaceCompare(self, s: str, t: str):
+        return self.op(s) == self.op(t)
+
+    def op(self, t):
+        ls_t = list()
+        for i in t:
+            if i == "#":
+                if ls_t:
+                    ls_t.pop()
+                continue
+            ls_t.append(i)
+        return ls_t
+
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+# #
+# a = "y#fo##f"
+# b = "y#f#o##f"
+# # a = "ab##"
+# # b = "c#d#"
+# Solution().backspaceCompare(a, b)
