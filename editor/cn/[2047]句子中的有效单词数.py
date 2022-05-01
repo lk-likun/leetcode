70e739a92826f99971f65b6506362445f920dfc3
lik <lk_likun@126.com>
2022-05-01 20:13:19 +0800

    20220501

---
+++ editor/cn/[2047]句子中的有效单词数.py
@@ -0,0 +1,13 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def countValidWords(self, sentence: str) -> int:
+        ls = sentence.split(" ")
+        res = 0
+        for i in ls:
+            pass
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+s = "alice and bob are playing stone-game10"
+Solution().countValidWords(s)
